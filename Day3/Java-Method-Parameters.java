01c650e1441221f7a28aa1be7392e09c6a724342
Sundram Singh <32551778+Annu1Singh@users.noreply.github.com>
2020-03-21 20:17:39 +0530

    Update and rename Java-Method-Parameters.txt to Java-Method-Parameters.java

--- Day3/Java-Method-Parameters.java
+++ Day3/Java-Method-Parameters.java
@@ -1,6 +1,5 @@
 /*Parameters and Arguments
 Information can be passed to methods as parameter. Parameters act as variables inside the method.
-
 Parameters are specified after the method name, inside the parentheses. You can add as many parameters as you want, just separate them with a comma.*/
 
 //Example
