ed01ab0d31fe2cb568ae2d89523eaa1bf6dc8193
Sundram Singh <32551778+Annu1Singh@users.noreply.github.com>
2020-03-21 20:16:58 +0530

    Update Java-2D-Array.java

--- Day3/Java-2D-Array.java
+++ Day3/Java-2D-Array.java
@@ -1,4 +1,4 @@
-class MyClass {
+class DemoClass {
   public static void main(String[] args) {
     int[][] num = { {1, 2, 3, 4}, {5, 6, 7} };
     int x = num[1][2];
@@ -6,7 +6,7 @@ class MyClass {
     //Loop Through an 2D Array
     for (int i = 0; i < num.length; ++i) {
       for(int j = 0; j < num[i].length; ++j) {
-        System.out.println(myNumbers[i][j]);
+        System.out.println(num[i][j]);
       }
     }
   }
